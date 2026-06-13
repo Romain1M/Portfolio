5b03565ac58d804f0cfe452686c570b00e95e27d
RomainMissoum <romain.missoum@gmail.com>
2026-06-14 00:46:23 +0200

    Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://romain1m.github.io',
+  base: '/Portfolio',
+});
