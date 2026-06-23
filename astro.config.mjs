3bd84007ffc406b0b4de4b0a72a68979b1840c2f
Romain Missoum <romain.missoum@orange.com>
2026-06-24 01:01:14 +0200

    Update portfolio layout and content; enhance styles and add new skills

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://romain1m.github.io',
-  base: '/Portfolio',
+  base: '/',
 });
